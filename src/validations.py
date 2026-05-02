de04d5c0e4f7c06e5c46c96fdba71387145cc474
ihateb335 <zhar.misha@gmail.com>
2026-05-02 14:16:07 +0300

    Merge Task-4 into main
    commit 971dc7b35230cb6372e5c70eb9277616a4d6659f
    Author: ihateb335 <zhar.misha@gmail.com>
    Date:   Sat May 2 14:15:38 2026 +0300
    
        Add docs for script
    
    commit e74130cfc167e444e12437ebbc70ffd38c6f3298
    Author: ihateb335 <zhar.misha@gmail.com>
    Date:   Sat May 2 14:14:26 2026 +0300
    
        Add hack when script is main
    
    commit 9dc9766b16b27cd25deaf9f626893054042231f2
    Author: ihateb335 <zhar.misha@gmail.com>
    Date:   Sat May 2 14:09:39 2026 +0300
    
        Add contacts bot
    
    commit 00d59155efd77f04c8278e54492c68374f9f4c94
    Author: ihateb335 <zhar.misha@gmail.com>
    Date:   Sat May 2 13:57:00 2026 +0300
    
        Add tests
    
    commit 0078ae41646bb08a39fb7a036715ac981b5ca694
    Author: ihateb335 <zhar.misha@gmail.com>
    Date:   Sat May 2 13:30:31 2026 +0300
    
        Add new validations

--- src/validations.py
+++ src/validations.py
@@ -1,19 +1,31 @@
 import re
 
 
-def validate_name(name: str) -> bool:
+def validate_name(name: str, no_spaces: bool = False) -> bool:
     """
     Validate the name of a living being.
 
     Args:
         name (str): The name of a living being.
-
+        no_spaces (bool, optional): Whether to allow spaces in the name. Defaults to False.
     Returns:
         bool: True if the name is valid, False otherwise.
     """
+    if no_spaces:
+        return re.match(r"^[\w]+$", name) is not None
 
     # Checks whether the name consists of letters, spaces and digits
     return re.match(r"^[\w\s]+$", name) is not None
+
+
+def validate_phone(phone: str) -> bool:
+    """
+    Validate the phone number of a living being.
+
+    Args:
+        phone (str): The phone number of a living being.
+    """
+    return re.match(r"^\d+$", phone) is not None
 
 
 def validate_positive_integer(integer: str) -> bool:
